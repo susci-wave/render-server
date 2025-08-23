62f0a548d4bf8efb5d541e6b495261a862323532
susci-wave <28866337+susci-wave@users.noreply.github.com>
2025-08-23 16:25:13 +0800

    change to rate

--- site/fund/pages/page/index.js
+++ site/fund/pages/page/index.js
@@ -73,10 +73,17 @@ function mergeNet2(jjset_set){
             jjset_set.jjset[key].net[jjset_set.netset.length - idx - 1] = val;
         })
     })
-     
+    
     let result = Object.values(jjset_set.jjset);
     result = result.filter(el=>el.net.findIndex(net=>!net) == -1);
     result.forEach(el=>{
+        // let firstNet = el.net.shift();
+        // el.net = el.net.map(net=> ((net-firstNet)/net*100).toFixed(2));
+        el.net = el.net.map((net,idx)=>{
+            if(idx == 0) return 0;
+            return (net - el.net[0])/el.net[0]*100;
+        }).map(net=>net.toFixed(2));
+
         calcNet(el);
     })
     return result;
@@ -84,68 +91,72 @@ function mergeNet2(jjset_set){
 
 let jj_feature_fn = [
     {
-        name: 'netMax',
-        text: '净值最大值',
+        name: 'rateMax',
+        text: '区间高点',
+        desc: '区间高点',
         fn: jj => Math.max(...jj.net),
         hide: true
     },
     {
-        name: 'netMin',
-        text: '净值最小值',
+        name: 'rateMin',
+        text: '区间低点',
+        desc: '区间低点',
         fn: jj => Math.min(...jj.net),
         hide: true
-    }, 
+    },
     {
-        name: 'netMaxMinusMin',
-        text: '区间波动',
+        name: 'rateRange',
+        text: '区间幅度',
+        desc: '区间高点-区间低点',
         fn: jj => {
-            return ((jj.feature.netMax - jj.feature.netMin)/jj.feature.netMin*100).toFixed(2);
+            return jj.feature.rateMax - jj.feature.rateMin;
         },
-        hide: true
     },
     {
-        name: 'netLatestMinusMin',
+        name: 'curRate2Min',
         text: '当前区间涨幅',
+        desc: '昨日涨幅-区间低点',
         fn: jj=>{
-            return ((jj.net[jj.net.length-1] - jj.feature.netMin)/jj.feature.netMin*100).toFixed(2);
+            return jj.net[jj.net.length-1] - jj.feature.rateMin;
         }
     },
     {
-        name: 'netLatestToMax',
+        name: 'curRate2Max',
         text: '当前区间跌幅',
+        desc: '昨日涨幅-区间高点',
         fn: jj => {
-            return  (jj.feature.netMaxMinusMin - jj.feature.netLatestMinusMin).toFixed(2);
+            return jj.net[jj.net.length-1] - jj.feature.rateMax;
         }
     },
     {
-        name: 'latestRate',
+        name: 'curRate',
         text: '昨日涨幅',
         fn: jj=>{
-            return ((jj.net[jj.net.length-1] - jj.net[jj.net.length-2])/jj.net[jj.net.length-2]*100).toFixed(2);
+            return jj.net[jj.net.length-1] - jj.net[jj.net.length-2];
         }
     },
     {
-        name: 'calc5DayRate',
-        text: '5日区间波动',
+        name: 'day5RateRange',
+        text: '5日区间幅度',
+        desc: '5日区间高点-5日区间低点',
         fn: jj=>{
             let net = jj.net;
-            let latest = net[net.length-1];
-            let min = Math.min(net[net.length-1], net[net.length-2], net[net.length-3], net[net.length-4], net[net.length-5]);
-            return ((latest - min) / min * 100).toFixed(2);
+            return Math.max(...net.slice(net.length-5))
+                - Math.min(...net.slice(net.length-5));
         }
     },
     {
-        name: 'calcUpDayCnt',
+        name: 'dayUpCnt',
         text: '连涨天数',
         fn: calcUpDayCnt
     },
     {
-        name: 'calc5DayRateUpRate',
+        name: 'day5UpRateRate',
         text: '5日上涨幅度比例',
         fn: calc5DayRateUpRate
     },
     {
-        name: 'calc5DayUpRate',
+        name: 'day5UpRate',
         text: '5日上涨天数比例',
         fn: calc5DayUpRate
     }
@@ -156,8 +167,25 @@ let jj_feature_fn = [
 function calcNet(jj){
     jj.feature = {}
     jj_feature_fn.map(el=>{
-        el.fn && (jj.feature[el.name] = el.fn.call(null, jj));
+        if(el.fn){
+            jj.feature[el.name] = el.fn.call(null, jj);
+            if(typeof jj.feature[el.name] == 'number'){
+                jj.feature[el.name] = jj.feature[el.name].toFixed(2);
+            }
+        }
     })
+}
+
+// 计算连续上涨天数
+function calcUpDayCnt(jj){
+    let cnt = 0;
+    for(let i=jj.net.length-1; i>0; i--){
+        if(jj.net[i] >= jj.net[i-1]){
+            cnt++;
+        }else{
+            return cnt;
+        }
+    }
 }
 
 // 计算5天上涨的比例
@@ -176,24 +204,12 @@ function calc5DayRateUpRate(jj){
     return (add / (add - min) * 100).toFixed(2);
 }
 
-// 计算连续上涨天数
-function calcUpDayCnt(jj){
-    let cnt = 0;
-    for(let i=jj.net.length-1; i=>0; i--){
-        if(jj.net[i] >= jj.net[i-1]){
-            cnt++;
-        }else{
-            return cnt;
-        }
-    }
-}
-
 // 计算5天上涨天数比例
 function calc5DayUpRate(jj){
     let net = jj.net;
     let upCount = 0;
     for (let i = net.length - 1; i > net.length - 6 && i >= 1; i--) {
-        if (net[i] >= net[i - 1]) {
+        if(jj.net[i] >= jj.net[i-1]){
             upCount++;
         }
     }
